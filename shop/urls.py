400a23f7fbd63dc6b9ac36ac148eb8d6b013aa75
Rudra616 <rudrampanchal@gmail.com>
2025-08-26 17:33:03 +0530

    add routes for User auth, Product, Cart, Order, and Payment endpoints

--- shop/urls.py
+++ shop/urls.py
@@ -1,5 +1,36 @@
 from django.urls import path
-from .views import heloWorld
+from .views import (
+    UserRegistrationView,
+    UserLoginView,
+    UserDetailView,
+    UserLogoutView,
+    ProductListCreateView,
+    ProductDetailView,
+    OrderListCreateView,
+    OrderDetailView,
+    PaymentCreateView,
+    CartListCreateView, CartDetailView
+)
+
 urlpatterns = [
-    path('',heloWorld)
+    # User Auth
+    path("register/", UserRegistrationView.as_view(), name="register"),
+    path("login/", UserLoginView.as_view(), name="login"),
+    path("user/", UserDetailView.as_view(), name="user-detail"),
+    path("logout/", UserLogoutView.as_view(), name="logout"),
+
+    # Products
+    path("products/", ProductListCreateView.as_view(), name="product-list-create"),
+    path("products/<int:pk>/", ProductDetailView.as_view(), name="product-detail"),
+
+
+
+    path('orders/', OrderListCreateView.as_view(), name='order-list-create'),
+    path('orders/<int:pk>/', OrderDetailView.as_view(), name='order-detail'),
+
+    # Payments
+    path('payments/', PaymentCreateView.as_view(), name='payment-create'),
+
+    path('cart/', CartListCreateView.as_view(), name='cart-list-create'),
+    path('cart/<int:pk>/', CartDetailView.as_view(), name='cart-detail'),
 ]

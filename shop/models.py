85ec8cb8c50382d1b3c3ec4fba279906b043dc83
Rudra616 <rudrampanchal@gmail.com>
2025-09-11 08:45:54 +0530

    commit

--- shop/models.py
+++ shop/models.py
@@ -1,6 +1,7 @@
 # models.py
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from cloudinary.models import CloudinaryField
 
 # ✅ User model
 class User(AbstractUser):
@@ -20,7 +21,7 @@ class User(AbstractUser):
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    image = models.ImageField(upload_to="category/", blank=True, null=True)
+    image = CloudinaryField("image", blank=True, null=True)
 
     def __str__(self):
         return self.name
